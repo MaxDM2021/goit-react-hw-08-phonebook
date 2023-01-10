92a9b4481413185d847af118d7dd8bda9abc0cce
MaxDM2021 <95755128+MaxDM2021@users.noreply.github.com>
2023-01-10 20:27:16 +0000

    Deploying to gh-pages from @ MaxDM2021/goit-react-hw-08-phonebook@eabc1f4c83411d7b3217e6d5cf092898f8035053 🚀

---
+++ static/js/822.d9c13fb9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[822],{9822:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});n(2791);var i=n(184),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",justifyContent:"center",margin:"50px"},title:{fontWeight:700,fontSize:48,textAlign:"center",color:"#2A363B"}},c=function(){return(0,i.jsx)("div",{style:r.container,className:"Homepage ",children:(0,i.jsx)("h1",{style:r.title,children:"PHONE-DATA STORAGE"})})}}}]);
+//# sourceMappingURL=822.d9c13fb9.chunk.js.map
